47f96c752d027f7b016d1983fbe45c80a4c4d44a
Aiperi Ormonbekova <aiperi.ormonbekovna@gmail.com>
2023-12-12 19:10:36 +0600

    added counter count

--- counter_bloc/lib/cubit/counter_cubit.dart
+++ counter_bloc/lib/cubit/counter_cubit.dart
@@ -3,7 +3,6 @@ import 'package:bloc/bloc.dart';
 class CounterCubit extends Cubit<int> {
   CounterCubit() : super(0); // баштапкы абалы
 
-  void increment() {
-    emit(4);
-  }
+  increment() => emit(state + 1);
+  decrement() => emit(state - 1);
 }
--- counter_bloc/lib/view/home_view.dart
+++ counter_bloc/lib/view/home_view.dart
@@ -13,14 +13,39 @@ class HomeView extends StatelessWidget {
         centerTitle: true,
       ),
       body: Center(
-        child: Text(context.watch<CounterCubit>().state.toString()),
-        // Text(context.watch<CounterCubit>().state.toString()), => Counter_cubit.dart файлндагы <CounterCubit> алып келип берет
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          context.read<CounterCubit>().increment();
-        },
-        child: const Icon(Icons.add),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              context.watch<CounterCubit>().state.toString(),
+              style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            // Text(context.watch<CounterCubit>().state.toString()), => Counter_cubit.dart файлндагы <CounterCubit> алып келип берет
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    context.read<CounterCubit>().increment();
+                  },
+                  child: const Icon(Icons.add),
+                ),
+                const SizedBox(
+                  width: 50,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    context.read<CounterCubit>().decrement();
+                  },
+                  child: const Icon(Icons.remove),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
