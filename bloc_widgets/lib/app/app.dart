11bf4db79dde114d3100ba27092e2df6657aaccd
Aiperi Ormonbekova <aiperi.ormonbekovna@gmail.com>
2024-01-06 18:39:54 +0600

    added

--- bloc_widgets/lib/app/app.dart
+++ bloc_widgets/lib/app/app.dart
@@ -1,19 +1,26 @@
+import 'package:bloc_widgets/logic/home_state.dart';
 import 'package:bloc_widgets/view/home_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  //BlocProvider => cubit же bloc менен камсыздоочу
+  // create => кайсыл cubit же bloc менен камсыздаганын беребиз
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return BlocProvider(
+      create: (context) => HomeCubit(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const HomeView(),
       ),
-      home: const HomeView(),
     );
   }
 }
--- bloc_widgets/lib/view/home_view.dart
+++ bloc_widgets/lib/view/home_view.dart
@@ -1,4 +1,6 @@
+import 'package:bloc_widgets/logic/home_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -9,6 +11,20 @@ class HomeView extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Bloc widget'),
         centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                final homeCubit = context.read<HomeCubit>();
+                homeCubit.parseNumber('214');
+              },
+              child: const Text('parse'),
+            ),
+          ),
+        ],
       ),
     );
   }
